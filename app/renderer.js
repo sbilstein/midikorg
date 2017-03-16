d9879a9fd077ecb596fc7c278f91e69a4561fdda
Siegfried Bilstein <sbilstein@linkedin.com>
2017-03-16 15:40:43 -0700

    initial commit, simple electron app that renders a React component

---
+++ app/renderer.js
@@ -0,0 +1,8 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../views/components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));

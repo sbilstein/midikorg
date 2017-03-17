ab81a17dc6b76a3669491decf6e32f9ac57d4122
Peter Hayes <peter.k.hayes@gmail.com>
2017-03-16 18:21:23 -0700

    added knob component demo

--- views/components/App.jsx
+++ views/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Knob from 'react-canvas-knob';
 
 export default class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {knobVal: 0};
+  }
+
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+      <div style={{textAlign: 'center'}}>
+        <h1>Microkorg</h1>
+        <Knob
+          title="Mod Wheel"
+          width={30}
+          value={this.state.knobVal}
+          onChange={(val) => this.setState({knobVal: val})}
+          onChangeEnd={(val) => console.log("Ended at val", val)}
+        />
+      </div>
+    );
   }
 }
